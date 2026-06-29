e083e7b137a9295799eddcaf16a734d441c642d7
anc <anastasishatzis@gmail.com>
2026-06-29 23:04:46 +0300

    chore: update API base URL and PascalCase market name for production

--- src/app/config/site.config.ts
+++ src/app/config/site.config.ts
@@ -3,8 +3,8 @@ export type Culture = 'el' | 'en';
 
 export const SITE_CONFIG = {
   api: {
-    baseUrl: 'http://localhost:5007',
-    market: 'trattoria-vico',
+    baseUrl: 'https://marketswebapi-gde3hpftfdhuawaj.westeurope-01.azurewebsites.net',
+    market: 'TrattoriaVico',
   },
   theme: {
     default: 'light' as Theme,
